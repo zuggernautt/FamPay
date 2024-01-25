9ca0003dc62f3b21c754e9d33430f55f75efb1bf
zuggernautt <amitak0707@gmail.com>
2024-01-26 01:07:08 +0530

    models

--- fampay/api/models.py
+++ fampay/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    published_at = models.DateTimeField()
+    thumbnail_url = models.URLField()
+    video_url = models.URLField()
+    channel_title = models.CharField(max_length=100)
+    channel_id = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = "Videos"
+        ordering = ['-published_at']
+        
